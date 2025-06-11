2ead1fe8fd5adbd9d7314570f17809f6188fc8a5
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2025-06-11 11:17:14 +0200

    fix: remove test border from Header component

--- src/components/homepage/Header/Header.tsx
+++ src/components/homepage/Header/Header.tsx
@@ -5,7 +5,7 @@ import { ListLinks } from "./ListLinks";
 
 export function Header() {
   return (
-    <header className="flex justify-between lg:justify-around lg:gap-12 py-4 px-4 border-b-2 border-amber-300">
+    <header className="flex justify-between lg:justify-around lg:gap-12 py-4 px-4">
       <div className="flex gap-2 md:gap-4 lg:gap-6">
         <button className="flex lg:hidden text-xl items-center pt-1 lg:pt-[1px]">
           <FontAwesomeIcon icon={faBars} />
