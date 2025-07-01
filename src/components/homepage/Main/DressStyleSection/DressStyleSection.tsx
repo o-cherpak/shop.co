937c0b6aa2868c8f5a68ea2f5b7b8449ff53014f
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2025-07-01 11:29:37 +0200

    feat: Refactor DressStyleSection layout for improved structure and responsiveness

--- src/components/homepage/Main/DressStyleSection/DressStyleSection.tsx
+++ src/components/homepage/Main/DressStyleSection/DressStyleSection.tsx
@@ -3,17 +3,19 @@ import { TopDressDiv } from "./TopDressDiv";
 
 export function DressStyleSection() {
   return (
-    <section className="mx-2 mt-10 px-6 py-10 flex flex-col justify-center items-center bg-gray-150 rounded-2xl">
-      <h2
-        style={{ fontFamily: "Integral CF", fontWeight: "bold" }}
-        className="text-4xl text-center pb-6"
-      >
-        BROWSE BY DRESS STYLE
-      </h2>
+    <section className="mx-2 mt-10 px-4 md:px-24 py-10 flex justify-center">
+      <div className="w-full py-6 px-4 flex flex-col justify-center items-center bg-gray-150 rounded-2xl">
+        <h2
+          style={{ fontFamily: "Integral CF", fontWeight: "bold" }}
+          className="text-4xl text-center pb-6"
+        >
+          BROWSE BY DRESS STYLE
+        </h2>
 
-      <TopDressDiv />
+        <TopDressDiv />
 
-      <BottomDressDiv />
+        <BottomDressDiv />
+      </div>
     </section>
   );
 }
