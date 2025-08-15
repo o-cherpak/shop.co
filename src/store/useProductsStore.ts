1f6c0573670144770a49bc4941848acdd78c0961
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2025-08-15 21:57:42 +0200

    add useProductsStore.ts (Zustand)

---
+++ src/store/useProductsStore.ts
@@ -0,0 +1,36 @@
+import {create} from 'zustand'
+import {child, get, ref} from "firebase/database";
+import {database} from "../../firebase-config.ts";
+import type {Product} from "../interfaces/Products.ts";
+
+interface ProductsState {
+  products: Product[] | null;
+  loadingProducts: boolean;
+  error: string | null;
+  fetchProducts: () => Promise<void>;
+}
+
+export const useProductsStore = create<ProductsState>((set) => ({
+  products: null,
+  loadingProducts: false,
+  error: null,
+
+  fetchProducts: async () => {
+    set({loadingProducts: true, error: null});
+
+    try {
+      const dbRef = ref(database);
+      const snapshot = await get(child(dbRef, "products"));
+
+      if (snapshot.exists()) {
+        const dataObj = snapshot.val();
+        const productsArray = Object.values(dataObj);
+        set({products: productsArray as Product[], loadingProducts: false});
+      } else {
+        set({products: [], loadingProducts: false});
+      }
+    } catch (err) {
+      set({error: (err as Error).message, loadingProducts: false});
+    }
+  }
+}))
