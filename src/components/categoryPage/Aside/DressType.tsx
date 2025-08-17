885d67610c759f5904a66522e059a33e717d656e
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2025-08-17 19:26:37 +0200

    add color in buttons

--- src/components/categoryPage/Aside/DressType.tsx
+++ src/components/categoryPage/Aside/DressType.tsx
@@ -1,24 +1,32 @@
-import {  faChevronRight } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faChevronRight} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {useFilteredProductsStore} from "../../../store/useFilteredProductsStore.ts";
 
 export function DressType() {
+  const {setType} = useFilteredProductsStore();
+
   return (
-    <div className="w-full pb-4 text-black/60 relative after:bottom-0 after:left-0 after:absolute after:content-[''] after:w-full after:h-[1px] after:bg-gray-300">
+    <div
+      className="w-full pb-4 text-black/60 relative after:bottom-0 after:left-0 after:absolute after:content-[''] after:w-full after:h-[1px] after:bg-gray-300">
       <ul className="w-full flex flex-col gap-2">
         <li className="flex justify-between items-center">
-          <p>Body</p> <span><FontAwesomeIcon icon={faChevronRight} /></span>
+          <button onClick={() => setType("Body")}>Body</button>
+          <span><FontAwesomeIcon icon={faChevronRight}/></span>
         </li>
 
         <li className="flex justify-between items-center">
-          <p>Legs</p> <span><FontAwesomeIcon icon={faChevronRight} /></span>
+          <button onClick={() => setType("Legs")}>Legs</button>
+          <span><FontAwesomeIcon icon={faChevronRight}/></span>
         </li>
 
         <li className="flex justify-between items-center">
-          <p>Boots</p> <span><FontAwesomeIcon icon={faChevronRight} /></span>
+          <button onClick={() => setType("Boots")}>Boots</button>
+          <span><FontAwesomeIcon icon={faChevronRight}/></span>
         </li>
 
         <li className="flex justify-between items-center">
-          <p>Other</p> <span><FontAwesomeIcon icon={faChevronRight} /></span>
+          <button onClick={() => setType("Other")}>Other</button>
+          <span><FontAwesomeIcon icon={faChevronRight}/></span>
         </li>
       </ul>
     </div>
