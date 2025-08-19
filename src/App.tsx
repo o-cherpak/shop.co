b8332c59a72a3323a86ed87e70e59e8f158ba0fa
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2025-08-19 14:39:04 +0200

    add smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,33 @@ import {HomePage} from "./pages/HomePage";
 import {CategoryPage} from "./pages/CategoryPage";
 import {ProductPage} from "./pages/ProductPage";
 import {CartPage} from "./pages/CartPage.tsx";
+import Lenis from "lenis";
+import {useEffect} from "react";
 
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => lenis.destroy();
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<HomePage/>}/>
       <Route path="/CategoryPage" element={<CategoryPage/>}/>
       <Route path="/CategoryPage/:productTitle" element={<ProductPage/>}/>
-      <Route path="/CartPage" element={<CartPage/>} />
+      <Route path="/CartPage" element={<CartPage/>}/>
     </Routes>
   );
 }
