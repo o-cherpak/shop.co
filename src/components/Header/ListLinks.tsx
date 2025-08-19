2fd0b5e7c8046f9d53aa04fede87bfd288e2d0ad
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2025-08-19 15:27:55 +0200

    add links hover effect in ListLinks.tsx

--- src/components/Header/ListLinks.tsx
+++ src/components/Header/ListLinks.tsx
@@ -1,26 +1,30 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import {useSortingStore} from "../../store/useSortingStore.ts";
 
 export function ListLinks() {
   const {setSortingOption} = useSortingStore();
 
+  const StyleForNavigationHoverAndActive = "after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] " +
+    "after:bg-black transition-all after:duration-500 hover:after:w-full after:rounded-full active:scale-95";
+
+
   return (
     <ul className="hidden lg:flex items-center gap-6">
-      
+
       <Link to="/CategoryPage" onClick={() => setSortingOption("Default")}>
-        <li className="text-lg">Shop</li>
+        <li className={`relative text-lg cursor-pointer ${StyleForNavigationHoverAndActive}`}>Shop</li>
       </Link>
 
       <Link to="/CategoryPage">
-        <li className="text-lg">On Sale</li>
+        <li className={`relative text-lg cursor-pointer ${StyleForNavigationHoverAndActive}`}>On Sale</li>
       </Link>
 
       <Link to="/CategoryPage" onClick={() => setSortingOption("New products")}>
-        <li className="text-lg">New Arrivals</li>
+        <li className={`relative text-lg cursor-pointer ${StyleForNavigationHoverAndActive}`}>New Arrivals</li>
       </Link>
 
       <Link to="/CategoryPage">
-        <li className="text-lg">Brands</li>
+        <li className={`relative text-lg cursor-pointer ${StyleForNavigationHoverAndActive}`}>Brands</li>
       </Link>
     </ul>
   );
