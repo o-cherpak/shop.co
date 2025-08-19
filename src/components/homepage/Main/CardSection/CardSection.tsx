114bfdd57bd8c53f281817eaa3526291c80484d5
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2025-08-19 23:58:30 +0200

    fix the undefined problem of sortingOption in CardSection.tsx

--- src/components/homepage/Main/CardSection/CardSection.tsx
+++ src/components/homepage/Main/CardSection/CardSection.tsx
@@ -7,7 +7,7 @@ import {hoverBgColor} from "../../../../constants/colors.ts";
 type NewArrivalsSectionProps = {
   products: Product[];
   title: string;
-  sortingOption: string;
+  sortingOption?: string;
 };
 
 export function CardSection({ products, title, sortingOption }: Readonly<NewArrivalsSectionProps>) {
@@ -25,7 +25,7 @@ export function CardSection({ products, title, sortingOption }: Readonly<NewArri
       <Cards products={products} />
 
       <div className="flex justify-center">
-        <Link to={"/CategoryPage"} onClick={() => setSortingOption(sortingOption)}>
+        <Link to={"/CategoryPage"} onClick={() => sortingOption && setSortingOption(sortingOption)}>
           <button
             className={`mt-1 w-full max-w-xs md:max-w-[200px] border border-black/40 p-2 px-6 
               rounded-3xl shadow-2xl cursor-pointer hover:scale-110 
